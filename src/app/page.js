fdd65ec1cdd5990f471435f5a1910c55a123946f
phuennae <liverpoolnae@gmail.com>
2023-09-18 15:28:37 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -36,14 +36,24 @@ export default function Home() {
   };
 
   const loadMyCourses = async () => {
+    setLoadingMyCourses(true);
     const resp = await axios.get("/api/enrollment", {
       headers: { Authorization: `Bearer ${token}` },
     });
     setMyCourses(resp.data.courses);
+
+    setLoadingMyCourses(false);
   };
 
   useEffect(() => {
     loadCourses();
+
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
+    if (token && username) {
+      setToken(token);
+      setAuthenUsername(username);
+    }
   }, []);
 
   useEffect(() => {
@@ -54,11 +64,15 @@ export default function Home() {
 
   const login = async () => {
     try {
+      setLoadingLogin(true);
       const resp = await axios.post("/api/user/login", { username, password });
       setToken(resp.data.token);
       setAuthenUsername(resp.data.username);
       setUsername("");
       setPassword("");
+      localStorage.setItem("token", resp.data.token);
+      localStorage.setItem("username", resp.data.username);
+      setLoadingLogin(false);
     } catch (error) {
       if (error.response.data) {
         alert(error.response.data.message);
@@ -70,6 +84,9 @@ export default function Home() {
     setAuthenUsername(null);
     setToken(null);
     setMyCourses(null);
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
   };
 
   return (
@@ -105,7 +122,12 @@ export default function Home() {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
-              <Button onClick={login}>Login</Button>
+              {!loadingLogin && <Button onClick={login}>Login</Button>}
+              {loadingLogin && (
+                <Button onClick={login} disabled={true}>
+                  Login...
+                </Button>
+              )}
             </Group>
           )}
           {authenUsername && (
@@ -133,9 +155,13 @@ export default function Home() {
             ))}
 
           {/* Do something with below loader!! */}
-          <Loader variant="dots" />
+          {loadingMyCourses && <Loader variant="dots" />}
         </Paper>
-        <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer
+          year="2023"
+          fullName="Phuenpa Champasri"
+          studentId="650612090"
+        />
       </Stack>
     </Container>
   );
